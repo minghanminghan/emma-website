be25fdd05822c0b7932bc5a4f327fa7a4a247411
minghanminghan <andrewjiang6789@gmail.com>
2026-04-23 15:48:44 -0400

    lint

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,7 +3,7 @@ import { useOutletContext } from "react-router-dom"
 type OutletContext = { display: number }
 
 export default function Projects() {
-  const { display } = useOutletContext<OutletContext>()
+  // const { display } = useOutletContext<OutletContext>()
 
   return (
     <div>
